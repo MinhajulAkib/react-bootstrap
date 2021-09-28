769a572685a48d9d7d6a15e3b50780e784ba2707
MinhajulAkib <00akibswe@gmail.com>
2021-09-28 20:08:59 +0600

    bootstrap setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Card from './Components/Card/Card';
 
 function App() {
   const items =[
@@ -19,11 +20,23 @@ function App() {
     },
     {
       name:'first item', description:'jsadhfjhjadsjfdjas', img:'https://upload.wikimedia.org/wikipedia/commons/d/dd/Shaheed_minar_Roehl.jpg'
+    },
+  
+    {
+      name:'first item', description:'jsadhfjhjadsjfdjas', img:'https://upload.wikimedia.org/wikipedia/commons/d/dd/Shaheed_minar_Roehl.jpg'
+    },
+  
+    {
+      name:'first item', description:'jsadhfjhjadsjfdjas', img:'https://upload.wikimedia.org/wikipedia/commons/d/dd/Shaheed_minar_Roehl.jpg'
     }
   ]
   return (
     <div className="row row-cols-1 row-cols-md-4 g-4">
-      
+      {
+        items.map(item => <Card
+          item = {item}
+        ></Card>)
+      }
     </div>
   );
 }
