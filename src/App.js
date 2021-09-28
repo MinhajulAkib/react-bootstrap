7c5e7849defd483041e18bfd3c4b874a73cf03a6
MinhajulAkib <00akibswe@gmail.com>
2021-09-28 21:48:20 +0600

    news api data load and display show

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Spinner } from 'react-bootstrap';
+import { Card, Col, Row, Spinner } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import News from './Components/News/News';
 
 const App = () => {
+  const [news, setNews] = useState([])
+  useEffect(() =>{
+    fetch('https://newsapi.org/v2/everything?q=tesla&from=2021-08-28&sortBy=publishedAt&apiKey=ca0f78879475413da777cf542f0b8224')
+    .then(res => res.json())
+    .then(data => setNews(data.articles))
+  },[])
   return (
-    <div>
-      <Button variant = "danger">Click me</Button>
-      <br/>
-      <Spinner animation="border" role="status">
-  <span className="visually-hidden">Loading...</span>
-</Spinner>
+    <div className="App">
+      {
+       news.length === 0?
+        <Spinner animation="border" />
+        :
+      <Row xs={1} md={3} className="g-4">
+        {
+          news.map (nw => <News news = {nw}></News>)
+        }
+     </Row>
+     }
     </div>
   );
-};
+}
 
 export default App;
