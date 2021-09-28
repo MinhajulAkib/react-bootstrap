f10dcc31938e49783b81307046e8d660da02d72b
MinhajulAkib <00akibswe@gmail.com>
2021-09-28 19:59:14 +0600

    bootstrap setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const items =[
+    {
+      name:'first item', description:'jsadhfjhjadsjfdjas', img:'https://upload.wikimedia.org/wikipedia/commons/d/dd/Shaheed_minar_Roehl.jpg'
+    },
+    {
+      name:'first item', description:'jsadhfjhjadsjfdjas', img:'https://upload.wikimedia.org/wikipedia/commons/d/dd/Shaheed_minar_Roehl.jpg'
+    },
+    {
+      name:'first item', description:'jsadhfjhjadsjfdjas', img:'https://upload.wikimedia.org/wikipedia/commons/d/dd/Shaheed_minar_Roehl.jpg'
+    },
+    {
+      name:'first item', description:'jsadhfjhjadsjfdjas', img:'https://upload.wikimedia.org/wikipedia/commons/d/dd/Shaheed_minar_Roehl.jpg'
+    },
+    {
+      name:'first item', description:'jsadhfjhjadsjfdjas', img:'https://upload.wikimedia.org/wikipedia/commons/d/dd/Shaheed_minar_Roehl.jpg'
+    },
+    {
+      name:'first item', description:'jsadhfjhjadsjfdjas', img:'https://upload.wikimedia.org/wikipedia/commons/d/dd/Shaheed_minar_Roehl.jpg'
+    }
+  ]
   return (
-    <div className="App">
-     
+    <div className="row row-cols-1 row-cols-md-4 g-4">
+      
     </div>
   );
 }
